f57e11a61da92c16ae19fafd440182a660caef97
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-19 06:21:33 -0300

    configurando os models

--- receitas/models.py
+++ receitas/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Receita(models.Model):
     nome = models.CharField(max_length=255)
-    breve_descricao = models.TextField()
+    breve_descrica0 = models.TextField()
     ingredientes = models.TextField()
     modo_de_preparo = models.TextField()
     preco = models.DecimalField(decimal_places=2, max_digits=18)
-    imagens = models.ImageField(blank=True)
+    link_imagens = models.URLField(max_length=300)
 
     def __str__(self):
         return self.nome
