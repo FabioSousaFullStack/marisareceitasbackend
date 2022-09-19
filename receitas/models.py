e0a0d9468a01fa9c45911a352eb184f3e61938cc
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-19 14:31:32 -0300

    configurando os inputs do sistema

--- receitas/models.py
+++ receitas/models.py
@@ -4,10 +4,7 @@ from django.db import models
 class Receita(models.Model):
     nome = models.CharField(max_length=255)
     breve_descricao = models.TextField()
-    ingredientes = models.TextField()
-    modo_de_preparo = models.TextField()
-    preco = models.DecimalField(decimal_places=2, max_digits=18)
-    link_imagens = models.URLField(max_length=300)
+    link_do_video = models.URLField(max_length=300)
 
     def __str__(self):
         return self.nome
